c5d27001342179a45f266445735df16ef0852ecc
Raphi <raphael@smartive.ch>
2025-09-19 18:44:58 +0200

    feat: use button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import {Button} from "@ost-cas-fea-25-26/pp-design-system";
+
 export default function Home() {
-  return <div>Lets go team Put & Push!</div>;
+  return <div>Lets go team Put & Push!
+  <Button variant='outline'>hello button</Button>
+  </div>;
 }
