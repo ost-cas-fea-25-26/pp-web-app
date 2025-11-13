21bc0f2fca4a22d379bb71e2509710138c6f3b33
Raphi <raphael@smartive.ch>
2025-11-13 13:27:15 +0100

    feat: initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@ost-cas-fea-25-26/pp-design-system";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Button variant="destructive">Hello Button</Button>
+      <Button>Hello Button</Button>
     </div>
   );
 }
