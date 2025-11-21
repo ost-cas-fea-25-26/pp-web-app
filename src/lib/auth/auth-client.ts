e5131ef9d232c7a0ace38e429c0727b9c08c1a67
Raphi <raphael@smartive.ch>
2025-11-21 22:07:48 +0100

    new env var

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -2,10 +2,7 @@ import { createAuthClient } from "better-auth/client";
 import { genericOAuthClient } from "better-auth/client/plugins";
 
 export const baseURL =
-  process.env.NEXT_URL || process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : `http://localhost:${process.env.PORT ?? 3000}`;
-
+  process.env.NEXT_PUBLIC_NEXT_URL ?? "http://localhost:3000";
 export const CUSTOM_PROVIDER_ID = "zitadel";
 
 const authClient = createAuthClient({
