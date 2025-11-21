d4675726234ff22c169000c55262a5dff8e2304c
Raphi <raphael@smartive.ch>
2025-11-21 21:37:45 +0100

    fix pipeline

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,10 +1,7 @@
 import { HttpClient } from "./client/http-client";
 import { PostsApi } from "./posts/posts.api";
 
-const httpClient = new HttpClient(
-  process.env.API_URL ??
-    (process.env.CI === "true" ? "http://localhost" : undefined),
-);
+const httpClient = new HttpClient(process.env.API_URL);
 
 export const api = {
   posts: new PostsApi(httpClient),
