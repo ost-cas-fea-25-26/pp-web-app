a4945ccc8ef52e118802d9617ebbeef876b5ea35
Raphi <raphael@smartive.ch>
2025-11-28 22:33:06 +0100

    remove braces

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,12 +1,10 @@
 import { CurrentUserProfile } from "@/components/current-user-profile";
 import { Suspense } from "react";
 
-const ProfilePage = () => {
-  return (
-    <Suspense fallback={<p>Loading profile...</p>}>
-      <CurrentUserProfile />
-    </Suspense>
-  );
-};
+const ProfilePage = () => (
+  <Suspense fallback={<p>Loading profile...</p>}>
+    <CurrentUserProfile />
+  </Suspense>
+);
 
 export default ProfilePage;
