c2ce75cbb50c6f03d9989be5aba284bcd7c41f32
Raphi <raphael@smartive.ch>
2025-12-05 14:59:13 +0100

    error handling

--- src/components/create-post-button.tsx
+++ src/components/create-post-button.tsx
@@ -6,7 +6,15 @@ import { createPostAction } from "@/lib/actions/posts.actions";
 
 export const CreatePostButton: FC = () => {
   const handleClick = async () => {
-    await createPostAction("Rory McIlroy 💚");
+    const result = await createPostAction("Rory McIlroy 💚");
+
+    if (!result.success) {
+      // TODO: Replace with a nice toast notification
+      // e.g. https://ui.shadcn.com/docs/components/sonner
+
+      // eslint-disable-next-line no-alert
+      alert(`Failed to create post: ${result.error ?? "Unknown error"}`);
+    }
   };
 
   return <Button onClick={handleClick}>Create Post (Rory McIlroy 💚)</Button>;
