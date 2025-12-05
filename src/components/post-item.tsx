48d5a782d5b15652e9487e4e4c4041483da98bd4
Marc Oertle <marc@frontify.com>
2025-12-05 11:07:32 +0100

    feat: post list (#25)
    Co-authored-by: Raphi <raphael@smartive.ch>

---
+++ src/components/post-item.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { FC, ReactNode } from "react";
+import { Mumble, MumbleActions } from "@ost-cas-fea-25-26/pp-design-system";
+
+type PostItemProps = {
+  id: string;
+  content: ReactNode;
+  userName: string;
+  userHandle: string;
+  avatarSrc?: string;
+  comments: number;
+  likes: number;
+};
+
+export const PostItem: FC<PostItemProps> = ({
+  id,
+  content,
+  userName,
+  userHandle,
+  avatarSrc,
+  comments,
+  likes,
+}) => (
+  <Mumble
+    content={content}
+    avatarSrc={avatarSrc}
+    userName={userName}
+    userHandle={userHandle}
+    timestamp="2 hours ago"
+    size="m"
+    actions={
+      //Todo: add 'use client' to MumbleActions component in the design system so that we can remove 'use client' from this file
+      <MumbleActions
+        commentCounter={comments}
+        likeCounter={likes}
+        deepLink={`/mumble/${id}`}
+      />
+    }
+  />
+);
--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,8 +1,39 @@
 import { getPostsAction } from "@/lib/actions/posts.actions";
 import { FC } from "react";
+import { PostItem } from "./post-item";
+import { Post } from "@/lib/api/posts/posts.types";
 
-export const PostList: FC = async () => {
-  const posts = await getPostsAction();
+type PostListProps = {
+  filterByTags?: string[];
+  filterLikedBy?: string[];
+  filterByCreatorsIds?: string[];
+};
 
-  return <pre>{JSON.stringify(posts, null, 2)}</pre>;
+export const PostList: FC<PostListProps> = async ({
+  filterByTags,
+  filterLikedBy,
+  filterByCreatorsIds,
+}) => {
+  const posts = await getPostsAction({
+    creators: filterByCreatorsIds,
+    tags: filterByTags,
+    likedBy: filterLikedBy,
+  });
+
+  return (
+    <div className="flex flex-col gap-2">
+      {posts?.data?.data?.map((post: Post) => (
+        <PostItem
+          key={post.id ?? "key"}
+          id={post.id ?? "id"}
+          content={<p>{post.text}</p>}
+          userName={post.creator?.username ?? "TODO: Name"}
+          userHandle={post.creator?.username ?? "TODO: Handle"}
+          avatarSrc={post.creator?.avatarUrl ?? undefined}
+          comments={post.replies ?? 0}
+          likes={post.likes ?? 0}
+        />
+      ))}
+    </div>
+  );
 };
