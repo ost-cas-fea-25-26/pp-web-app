82ff048d531da3973ac298ba7d29ed4eba3f31f6
Raphi <raphael@smartive.ch>
2025-12-16 23:00:35 +0100

    fix pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: "npm run build:test && npm run start",
+    command: "npm run build && npm run start",
     url: "http://localhost:3000",
     timeout: 60_000,
     reuseExistingServer: !process.env.CI,
