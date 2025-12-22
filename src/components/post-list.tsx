e173ff06ea972cb57e94683de6c83325897dee29
Marc Oertle <marc@frontify.com>
2025-12-22 10:56:00 +0100

    load additional user data for posts

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -2,6 +2,7 @@ import { getPostsAction } from "@/lib/actions/posts.actions";
 import type { FC } from "react";
 import { PostItem } from "./post-item";
 import type { Post } from "@/lib/api/posts/posts.types";
+import { mapCreatorUserToUser } from "@/lib/mappers/user.mappers";
 
 type PostListProps = {
   filterByTags?: string[];
@@ -26,16 +27,20 @@ export const PostList: FC<PostListProps> = async ({
 
   const posts = postsResult.payload.data ?? [];
 
+  const users = await Promise.all(
+    posts.map((post) => mapCreatorUserToUser(post.creator ?? {})),
+  );
+
   return (
     <div className="flex flex-col gap-2">
-      {posts.map((post: Post) => (
+      {posts.map((post: Post, postIndex: number) => (
         <PostItem
           key={post.id ?? "key"}
           id={post.id ?? "id"}
           content={<p>{post.text}</p>}
-          userName={post.creator?.username ?? "TODO: Name"}
-          userHandle={post.creator?.username ?? "TODO: Handle"}
-          avatarSrc={post.creator?.avatarUrl ?? undefined}
+          userName={users[postIndex]?.fullName}
+          userHandle={users[postIndex]?.handle}
+          avatarSrc={users[postIndex]?.avatarUrl}
           comments={post.replies ?? 0}
           likes={post.likes ?? 0}
           liked={!!post.likedBySelf}
