0e7edba6ddccafe50ba07b9d5c64de00ca439733
Marc Oertle <marc@frontify.com>
2025-12-22 10:55:43 +0100

    create mapper for creator payload

--- src/lib/mappers/user.mappers.ts
+++ src/lib/mappers/user.mappers.ts
@@ -1,3 +1,6 @@
+import { getUserByIdAction } from "@/lib/actions/users.actions";
+import { User } from "@/lib/api/users/users.types";
+
 export type MumbleUser = {
   id: string;
   fullName: string;
@@ -5,13 +8,35 @@ export type MumbleUser = {
   avatarUrl?: string;
 };
 
-export const mapUserPayloadToUser = (user: {
+export const mapUserPayloadToUser = (user: User): MumbleUser => {
+  if (!user.id) {
+    throw new Error("User id is required");
+  }
+
+  if (!user.username) {
+    throw new Error("User username is required");
+  }
+
+  return {
+    id: user.id,
+    fullName: user.firstname + " " + user.lastname,
+    handle: user.username,
+    avatarUrl: user.avatarUrl ?? undefined,
+  };
+};
+
+export const mapCreatorUserToUser = async (creator: {
   readonly id?: string | null;
-  username?: string | null;
-  readonly avatarUrl?: string | null;
-  firstname?: string | null;
-  lastname?: string | null;
-}): MumbleUser => {
+}): Promise<MumbleUser> => {
+  if (!creator.id) {
+    throw new Error("User id is required");
+  }
+  const userResult = await getUserByIdAction(creator.id);
+  if (!userResult.success) {
+    throw new Error("Failed to fetch user");
+  }
+
+  const user = userResult.payload;
   if (!user.id) {
     throw new Error("User id is required");
   }
