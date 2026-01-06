1021279465754caf3ca11158d235c3a48a71e2e8
Raphi <raphael@smartive.ch>
2026-01-06 17:44:35 +0100

    fix build

--- next.config.ts
+++ next.config.ts
@@ -35,5 +35,5 @@ const nextConfig: NextConfig = {
 export default withSerwist({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
-  disable: process.env.NODE_ENV !== "production",
+  // disable: process.env.NODE_ENV !== "production",
 })(nextConfig);
